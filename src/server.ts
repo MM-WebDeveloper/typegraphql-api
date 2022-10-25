e9b2ea4aebd511695ffe4789a001f9e6d6a1faa8
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-10-25 21:51:50 +0200

    added redis and express-session

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import Express from 'express';
 import { buildSchema } from 'type-graphql';
 import { DataSource } from 'typeorm';
 import { RegisterResolver } from './modules/user/register';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { redis } from './redis';
+import cors from 'cors';
 
 const run = async () => {
 	const myDataSource = new DataSource(require('../ormconfig.json'));
@@ -12,9 +16,36 @@ const run = async () => {
 	const schema = await buildSchema({ resolvers: [RegisterResolver] });
 	const apolloServer = new ApolloServer({
 		schema,
+		context: (req) => ({ req }),
 	});
 
 	const app = Express();
+
+	const RedisStore = connectRedis(session);
+
+	app.use(
+		cors({
+			credentials: true,
+			origin: 'http://localhost:3000',
+		})
+	);
+
+	app.use(
+		session({
+			store: new RedisStore({
+				client: redis,
+			}),
+			name: 'qid',
+			secret: 'secret',
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				httpOnly: true,
+				secure: process.env.NODE_ENV === 'production',
+				maxAge: 1000 * 60 * 60 * 24 * 2 * 365, // 2 years
+			},
+		})
+	);
 
 	await apolloServer.start();
 
